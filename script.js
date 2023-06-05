8ae3e6d27720a84623672631ad6cc808f5cbe320
H-Ramalan <talktoramalan@gmail.com>
2023-06-05 13:47:02 -0700

    adding a book functionality

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 "use strict";
 
+// const storedBooks = localStorage.getItem("awesomeBooks");
+// let awesomeBooks = storedBooks ? JSON.parse(storedBooks) : [];
+
 const add = document.querySelector(".add");
+// const removeBtn = document.querySelectorAll(".remove-book");
 
 let books = [];
-let newBook = {
-  id: "",
-  title: "",
-  author: "",
+const bookslist = document.querySelector(".awesome-books-list");
+
+const clearFields = () => {
+  document.querySelector(".title").value = "";
+  document.querySelector(".author").value = "";
 };
 
-const addbook = (id, title, author) => {
-  newBook.id = Date.now.toString();
-  newBook.title = document.querySelector(".title").value;
-  newBook.author = document.querySelector(".author").value;
-  books.push(newBook);
-  e.preventDefault();
-  console.log("clicked");
+const addbook = (event) => {
+  let newbook = {
+    id: Date.now.toString(),
+    title: document.querySelector(".title").value,
+    author: document.querySelector(".author").value,
+  };
+  event.preventDefault();
+  books.push(newbook);
+  bookslist.innerHTML = "";
+
+  books.forEach((item) => {
+    const book = document.createElement("div");
+    book.classList.add("book");
+    book.innerHTML = `
+    <p class='book-title'>${item.title}</p>
+    <p class ='book-author'>${item.author}</p>
+    <button class='remove-book'>Remove</button>
+    <hr>
+  `;
+
+    bookslist.appendChild(book);
+  });
 };
 
 add.addEventListener("click", addbook);
+
+const deleteButtons = document.querySelectorAll(".remove-book");
+deleteButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    books.splice(index, 1);
+  });
+});
