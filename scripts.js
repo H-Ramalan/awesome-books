cd84301b9f0bb1a26780b87e8b2c911895790c2a
H-Ramalan <talktoramalan@gmail.com>
2023-06-07 06:41:09 -0700

    config linters

--- scripts.js
+++ scripts.js
@@ -7,35 +7,35 @@ class Book {
 
 class BookCollection {
   constructor() {
-    this.books = JSON.parse(localStorage.getItem("books")) || [];
-    this.bookList = document.getElementById("bookList");
-    this.bookForm = document.getElementById("bookForm");
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.bookList = document.getElementById('bookList');
+    this.bookForm = document.getElementById('bookForm');
 
-    this.bookForm.addEventListener("submit", this.handleFormSubmit.bind(this));
+    this.bookForm.addEventListener('submit', this.handleFormSubmit.bind(this));
 
     this.displayBookList();
   }
 
   removeBook(index) {
     this.books.splice(index, 1);
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
     this.displayBookList();
   }
 
   displayBookList() {
-    this.bookList.innerHTML = "";
+    this.bookList.innerHTML = '';
 
     this.books.forEach((book, index) => {
-      const row = document.createElement("tr");
+      const row = document.createElement('tr');
 
-      const bookCell = document.createElement("td");
+      const bookCell = document.createElement('td');
       bookCell.textContent = `"${book.title}" by ${book.author} `;
 
-      const removeCell = document.createElement("td");
-      const removeButton = document.createElement("button");
-      removeButton.className = "button";
-      removeButton.textContent = "Remove";
-      removeButton.addEventListener("click", () => {
+      const removeCell = document.createElement('td');
+      const removeButton = document.createElement('button');
+      removeButton.className = 'button';
+      removeButton.textContent = 'Remove';
+      removeButton.addEventListener('click', () => {
         this.removeBook(index);
       });
       removeCell.appendChild(removeButton);
@@ -50,19 +50,19 @@ class BookCollection {
   addBook(title, author) {
     const book = new Book(title, author);
     this.books.push(book);
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
     this.displayBookList();
   }
 
   handleFormSubmit(event) {
     event.preventDefault();
-    const titleInput = document.getElementById("title");
-    const authorInput = document.getElementById("author");
+    const titleInput = document.getElementById('title');
+    const authorInput = document.getElementById('author');
     const title = titleInput.value;
     const author = authorInput.value;
     this.addBook(title, author);
-    titleInput.value = "";
-    authorInput.value = "";
+    titleInput.value = '';
+    authorInput.value = '';
   }
 }
 
